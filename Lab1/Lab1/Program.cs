2a10057f7124403ecc5cf836bc50a091c4a7f438
andbolotin <33487574+andbolotin@users.noreply.github.com>
2017-12-27 16:20:53 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main_menu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine();
+            Console.WriteLine("1.Solve");
+            Console.WriteLine("2.Put in new equation");
+            Console.WriteLine("3.Exit");
+
+        }
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Program to solve equation");
+            Console.WriteLine("ax^2+bx+c=0");
+            int n = 0;
+
+            kv_equation.kv_equation_edit();
+            Console.WriteLine();
+
+            while (n != 3)
+            {
+
+                Main_menu();
+                n = int.Parse(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        {
+                            kv_equation.solv();
+                            break;
+                        }
+                    case 2:
+                        {
+                            kv_equation.kv_equation_edit();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Thank you for using this very program");
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("ERROR");
+                        }
+                        break;
+                }
+
+            }
+            return 0;
+        }
+
+    }
+
+}
---
+++ Lab1/Lab1/kv_equation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab1
+{
+    static class kv_equation
+    {
+        private static float A, B, C;
+        public static void kv_equation_edit()
+        {
+            bool mistak = false;
+            do
+            {
+                Console.Write("A= ");
+                A = float.Parse(Console.ReadLine());
+                Console.Write("B= ");
+                B = float.Parse(Console.ReadLine());
+                Console.Write("C= ");
+                C = float.Parse(Console.ReadLine());
+                if (A == 0)
+                {
+                    mistak = true;
+                    Console.WriteLine("Incorrect. Please try again.");
+                }
+                else
+                {
+                    mistak = false;
+                }
+            }
+            while (mistak == true);
+        }
+        public static void solv()
+        {
+            float D = B * B - 4 * A * C;
+            if (D < 0)
+                Console.WriteLine("There is no solutions");
+            else
+                if (D == 0)
+                Console.WriteLine(-B / (2 * A));
+            else
+            {
+                Console.WriteLine((-B + Math.Sqrt(D)) / (2 * A));
+                Console.WriteLine((-B - Math.Sqrt(D)) / (2 * A));
+            }
+
+        }
+    }
+}
